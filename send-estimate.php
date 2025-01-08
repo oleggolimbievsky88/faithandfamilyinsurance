dd7c5071561418a321fa530d2e2ebd1ffc6710af
Oleg Golimbievsky <oleg.golimbievsky@gmail.com>
2025-01-08 04:41:42 -0600

    Added Apple favicon

--- send-estimate.php
+++ send-estimate.php
@@ -1,48 +1,49 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Your email address
-    $to_email = "info@faithandfamilyinsurance.com";
+    // $to_email = "info@faithandfamilyinsurance.com,oleg.golimbievsky@gmail.com";
     
-    // Collect form data
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
-    $dob = $_POST['dob'];
-    $beneficiary = $_POST['beneficiary'];
-    $tobacco = $_POST['tobacco'];
-    $coverage = $_POST['coverage'];
-    $call_time = $_POST['call-time'];
-    $email = $_POST['email'];
-    $additional_details = $_POST['additional-details'];
+    // // Collect form data
+    // $name = $_POST['name'];
+    // $phone = $_POST['phone'];
+    // $address = $_POST['address'];
+    // $dob = $_POST['dob'];
+    // $beneficiary = $_POST['beneficiary'];
+    // $tobacco = $_POST['tobacco'];
+    // $coverage = $_POST['coverage'];
+    // $call_time = $_POST['call-time'];
+    // $email = $_POST['email'];
+    // $additional_details = $_POST['additional-details'];
     
-    // Email subject
-    $subject = "New Insurance Estimate Request from $name";
+    // // Email subject
+    // $subject = "New Insurance Estimate Request from $name";
     
-    // Email message
-    $message = "New Insurance Estimate Request:\n\n";
-    $message .= "Name: $name\n";
-    $message .= "Phone: $phone\n";
-    $message .= "Address: $address\n";
-    $message .= "Date of Birth: $dob\n";
-    $message .= "Beneficiary: $beneficiary\n";
-    $message .= "Tobacco Use: $tobacco\n";
-    $message .= "Coverage Amount: $$coverage\n";
-    $message .= "Preferred Call Time: $call_time\n";
-    $message .= "Email: $email\n";
-    $message .= "Additional Details: $additional_details\n";
+    // // Email message
+    // $message = "New Insurance Estimate Request:\n\n";
+    // $message .= "Name: $name\n";
+    // $message .= "Phone: $phone\n";
+    // $message .= "Address: $address\n";
+    // $message .= "Date of Birth: $dob\n";
+    // $message .= "Beneficiary: $beneficiary\n";
+    // $message .= "Tobacco Use: $tobacco\n";
+    // $message .= "Coverage Amount: $$coverage\n";
+    // $message .= "Preferred Call Time: $call_time\n";
+    // $message .= "Email: $email\n";
+    // $message .= "Additional Details: $additional_details\n";
     
-    // Headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "X-Mailer: PHP/" . phpversion();
+    // // Headers
+    // $headers = "From: $email\r\n";
+    // $headers .= "Reply-To: $email\r\n";
+    // $headers .= "X-Mailer: PHP/" . phpversion();
     
-    // Send email
-    if (mail($to_email, $subject, $message, $headers)) {
-        header("Location: confirmation.html"); // Redirect to confirmation page
-        exit;
-    } else {
-        echo json_encode(['success' => false, 'message' => 'Failed to send email']);
-    }
+    // // Send email
+    // if (mail($to_email, $subject, $message, $headers)) {
+    //     header("Location: confirmation.html"); // Redirect to confirmation page
+    //     exit;
+    // } else {
+    //     echo json_encode(['success' => false, 'message' => 'Failed to send email']);
+    // }
+    echo "test";
     exit;
 }
 ?> 
